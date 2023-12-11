1ac64b8840da293cb79f4738d8bc6b8e3f02662c
PUMOVETZ <batsiev2006@gmail.com>
2023-12-11 22:41:33 +0300

    Fix commit_ tah

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main() {
     Network net;
-    net.add_input_layer(2, Activation::sigmoid, 0.0f);
-    net.add_hidden_layer(4, Activation::tanh, 0.0f);
-    net.add_hidden_layer(6, Activation::fast_sigmoid, 0.0f);
-    net.add_output_layer(3, Activation::tanh, 0.0f);
+    net.add_input_layer(2, activation::sigmoid, 0.0f);
+    net.add_hidden_layer(4, activation::tanh, 0.0f);
+    net.add_hidden_layer(6, activation::fast_sigmoid, 0.0f);
+    net.add_output_layer(3, activation::tanh, 0.0f);
     net.build();
     net.show_network();
 
