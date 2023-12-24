c24103c7df3cf727a95baa6e0816859cd72a2155
kypluk44 <kypluk44@gmail.com>
2023-12-24 15:38:55 +0300

    add normalising weights

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "Network.h"
 #include "Activations.h"
+#include "DataUtils.h"
+
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <filesystem>
 
+
+
 int main() {
     std::filesystem::current_path("..");
+
 
     Network net;
     net.add_layer(LayerType::input, 2, FunctionType::relu, 0.0f);
@@ -15,13 +20,39 @@ int main() {
 
     net.build();
     net.show_network();
-    std::vector<std::vector<std::vector<float>>> input_data = {{{1.0f}, {2.0f}}, {{3.0f}, {4.0f}}};
-    std::vector<std::vector<float>> output_data = {{3.0f, 4.09f}, {7.0f, 8.07f}};
+
+    std::fstream input_file("input_data.txt");
+    std::vector<std::vector<std::vector<float>>> input_data;
+    std::vector<std::vector<float>> output_data;
+
+    float value;
+    while (input_file >> value) {
+        std::vector<std::vector<float>> input_vector;
+        input_vector.emplace_back(1, value);
+        input_file >> value;
+        input_vector.emplace_back(1, value);
+
+        input_data.push_back(input_vector);
+
+        std::vector<float> output_vector;
+        input_file >> value;
+        output_vector.push_back(value);
+        input_file >> value;
+        output_vector.push_back(value);
+
+        output_data.push_back(output_vector);
+
+    }
+    normalize_input(input_data);
+    normalize_output(output_data);
+
+    display_data(input_data, output_data);
+
 
     const int epochs = 1;
 
 
-    //net.train(input_data, output_data, epochs, 1, 0.1);
+    net.train(input_data, output_data, epochs, 1, 0.1);
 
     //net.save("base.json");
 
