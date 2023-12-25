ad29a96ed8dd6b84bb6870b98ae6e4db748f9f4c
kypluk44 <kypluk44@gmail.com>
2023-12-25 05:22:26 +0300

    add docs

--- Activ_derivatives.h
+++ Activ_derivatives.h
@@ -1,12 +1,38 @@
 #pragma once
 
+/**
+ * @file Activ_derivatives.h
+ * @brief Header file containing derivative functions for various activation functions.
+ */
+
 namespace Derivatives {
+
+    /**
+     * @brief Compute the derivative of the sigmoid activation function.
+     * @param x Input value.
+     * @return Derivative of the sigmoid function.
+     */
     float sigmoid_derivative(float x);
 
+    /**
+     * @brief Compute the derivative of the fast sigmoid activation function.
+     * @param x Input value.
+     * @return Derivative of the fast sigmoid function.
+     */
     float fast_sigmoid_derivative(float x);
 
+    /**
+     * @brief Compute the derivative of the silu (Sigmoid Linear Unit) activation function.
+     * @param x Input value.
+     * @return Derivative of the silu function.
+     */
     float silu_derivative(float x);
 
+    /**
+     * @brief Compute the derivative of the hyperbolic tangent (tanh) activation function.
+     * @param x Input value.
+     * @return Derivative of the tanh function.
+     */
     float tanh_derivative(float x);
 
 }
--- Neurons.h
+++ Neurons.h
@@ -1,15 +1,37 @@
 #pragma once
+
+/**
+ * @file Neurons.h
+ * @brief Header file containing the Neuron class for representing individual neurons.
+ */
+
 #include <functional>
 #include <utility>
 #include "Activations.h"
 
+/**
+ * @class Neuron
+ * @brief Represents an individual neuron in a neural network.
+ */
 class Neuron {
 public:
+    /**
+     * @brief Parameterized constructor to initialize a neuron.
+     * @param layer_number Layer index to which the neuron belongs.
+     * @param weight Weight assigned to the neuron.
+     * @param type Activation function type for the neuron.
+     */
     Neuron(int layer_number, float weight, FunctionType type);
 
+    /**
+     * @brief Compute the activation of the neuron for a given input.
+     * @param input Input value to the neuron.
+     * @return Activation value.
+     */
     float computeActivation(float input);
 
-    int layer_num;
-    float weight;
-    FunctionType fn_type;
+    int layer_num; ///< Layer index to which the neuron belongs
+    float weight; ///< Weight assigned to the neuron
+    FunctionType fn_type; ///< Activation function type for the neuron
 };
+
