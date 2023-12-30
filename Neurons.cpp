e45168fdbbdc0fd86e19390efa2d42112afbfc3a
PUMOVETZ <batsiev2006@gmail.com>
2023-12-30 22:07:54 +0300

    Fix neu

--- Neurons.cpp
+++ Neurons.cpp
@@ -3,6 +3,6 @@
 Neuron::Neuron(int layer_number, float weight, FunctionType type)
         : layer_num(layer_number), weight(weight), fn_type(type) {}
 
-float Neuron::computeActivation(float input) {
+[[maybe_unused]] float Neuron::computeActivation(float input) const {
     return call(fn_type, input);
 }
--- Neurons.h
+++ Neurons.h
@@ -3,13 +3,27 @@
 #include <utility>
 #include "Activations.h"
 
+/**
+ * @brief Class representing a Neuron in a neural network.
+ */
 class Neuron {
 public:
+    /**
+     * @brief Constructor for Neuron class.
+     * @param layer_number The layer number to which the neuron belongs.
+     * @param weight The weight of the neuron.
+     * @param type The activation function type for the neuron.
+     */
     Neuron(int layer_number, float weight, FunctionType type);
 
-    float computeActivation(float input);
+    /**
+     * @brief Computes the activation of the neuron based on the input.
+     * @param input The input value for computing the activation.
+     * @return The computed activation value.
+     */
+    [[maybe_unused]] [[nodiscard]] float computeActivation(float input) const;
 
-    int layer_num;
-    float weight;
-    FunctionType fn_type;
+    int layer_num; /**< The layer number of the neuron. */
+    float weight; /**< The weight of the neuron. */
+    FunctionType fn_type; /**< The activation function type of the neuron. */
 };
