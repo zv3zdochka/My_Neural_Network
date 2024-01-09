66d9c09cc8ddfb7c06f20413d0900a5e31f66968
PUMOVETZ <batsiev2006@gmail.com>
2024-01-09 22:54:08 +0300

    ini

--- main.cpp
+++ main.cpp
@@ -8,20 +8,31 @@ int main() {
     net.add_layer(LayerType::input, 2, FunctionType::fast_sigmoid);
     net.add_layer(LayerType::hidden, 3, FunctionType::sigmoid);
     net.add_layer(LayerType::hidden, 5, FunctionType::silu);
-    net.add_layer(LayerType::hidden, 7, FunctionType::sigmoid);
-    net.add_layer(LayerType::hidden, 3, FunctionType::tanh);
-    net.add_layer(LayerType::output, 2, FunctionType::sigmoid);
+    net.add_layer(LayerType::output, 1, FunctionType::sigmoid);
     net.build();
     net.show_network();
     net.save("base.json");
 
-    std::vector<std::vector<float>> input_data = {{0.6f, 0.2f}};
-    std::vector<std::vector<float>> output_data = {{1.0f, 0.3f}};
+    std::vector<std::vector<float>> input_data = {
+            {1.0f, 0.0f},
+            {1.0f, 1.0f},
+            {0.0f, 1.0f},
+            {0.0f, 0.0f},
+    };
 
-    const int epochs = 10000;
 
-    net.train(input_data, output_data, Normalisation::min_max_normalisation, epochs, 1, 0.1);
-    net.work(input_data);
+    std::vector<std::vector<float>> output_data = {
+            {0.0f},
+            {1.0f},
+            {1.0f},
+            {1.0f},
+    };
+
+    const int epochs = 1000;
+
+    net.train(input_data, output_data, Normalisation::without_normalisation, epochs, 1, 0.05);
+    net.work({{{1.0, 1.0}}});
+
 
     return 0;
 }
